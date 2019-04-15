374aae287586ce60afca8f05fb27056d4b8653c2
Joey-chn <yiming.civi@gmail.com>
2019-04-15 17:12:06 +0200

    feature create game

--- src/main/java/ch/uzh/ifi/seal/soprafs19/repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/repository/GameRepository.java
@@ -9,5 +9,4 @@ public interface GameRepository extends CrudRepository<Game, Long> {
     public Game findById(long id);
     public Game findByPlayer1orPlayer2(Long playerId);
     public Iterable<Game> findByGameMode(GameMode gameMode);      //  for lobby in the frontend
-
 }
