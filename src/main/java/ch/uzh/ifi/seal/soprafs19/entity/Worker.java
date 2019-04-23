79344248e34f232aff7d223acfbb430f3e60519c
Joey-chn <yiming.civi@gmail.com>
2019-04-23 18:51:07 +0200

    add equal methods in Worker

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/Worker.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/Worker.java
@@ -1,14 +1,17 @@
 package ch.uzh.ifi.seal.soprafs19.entity;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-public abstract class Worker {
+public abstract class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue
-    private  long id;  // make all the worker share one id
+    private  Long id;  // make all the worker share one id
 
     @Column
     private long playerId;
@@ -17,10 +20,21 @@ public abstract class Worker {
     public abstract void move(long fieldId);
     public abstract void build(long fieldId, int level);
 
-    public long getId() { return this.id; }
+    public Long getId() { return this.id; }
     public void setId(long id) {this.id = id;}
 
     public long getPlayerId() {return this.playerId;}
     public void setPlayerId(long playerId) {this.playerId = playerId; }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Worker)) {
+            return false;
+        }
+        Worker worker = (Worker) o;
+        return this.getId().equals(worker.getId());
+    }
 
 }
