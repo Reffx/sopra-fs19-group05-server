df437857c671f3676f2e91e7a6293e063b54835b
darni1605 <dominik.arni@uzh.ch>
2019-04-24 15:27:07 +0200

    G05-87: Finalized backend playboard

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/PlayfieldController.java
@@ -0,0 +1,28 @@
+package ch.uzh.ifi.seal.soprafs19.controller;
+
+import ch.uzh.ifi.seal.soprafs19.entity.Playfield;
+import ch.uzh.ifi.seal.soprafs19.service.PlayfieldService;
+import ch.uzh.ifi.seal.soprafs19.entity.Game;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class PlayfieldController {
+
+    private final PlayfieldService playfieldService;
+
+    private PlayfieldController(PlayfieldService playfieldService) {
+        this.playfieldService = playfieldService;
+    }
+
+    @PostMapping("/games/{gameId}")
+    Playfield newPlayfield(@PathVariable long gameId){
+        return playfieldService.initPlayfield(gameId);
+    }
+
+    @CrossOrigin
+    @PutMapping("playfield/{playfieldId}")
+    Playfield updatePlayfield(@RequestBody Playfield newPlayfield){ return this.playfieldService.updatePlayfield(newPlayfield);}
+}
