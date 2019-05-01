cb74d610225d958136a967dc80f388b2264f3d15
darni1605 <dominik.arni@uzh.ch>
2019-05-01 14:46:17 +0200

    G05-36: added get method for frontend to get highlighted fields

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/WorkerController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/WorkerController.java
@@ -2,10 +2,13 @@ package ch.uzh.ifi.seal.soprafs19.controller;
 
 import ch.uzh.ifi.seal.soprafs19.entity.Game;
 import ch.uzh.ifi.seal.soprafs19.entity.Player;
+import ch.uzh.ifi.seal.soprafs19.entity.Field;
 import ch.uzh.ifi.seal.soprafs19.entity.Worker;
 import ch.uzh.ifi.seal.soprafs19.service.WorkerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class WorkerController {
@@ -20,6 +23,11 @@ public class WorkerController {
     @PutMapping("/games/{gameId}/{playerId}/{workerId}/move")
     ResponseEntity<Worker> moveTo(@PathVariable(name = "gameId") long gameId, @PathVariable(name = "playerId") long playerId, @PathVariable(name = "workerId") int workerId, @RequestBody int dest) {
         return workerService.moveTo(gameId, playerId, workerId, dest);
+    }
+
+    @GetMapping("games/{gameId}/fieldNum/highlight")
+    ResponseEntity<List<Integer>> highlightField(@PathVariable(name = "gameId") long gameId, @PathVariable(name = "fieldNum") int fieldNum){
+        return workerService.highlightField(fieldNum, gameId);
     }
 
     //JUWE: Put mapping to place worker on field in initial round, afterwards move to method must be use
